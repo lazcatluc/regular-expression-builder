4938dcefe76077a0dc5c4032a67cbd21a9eb6946
catalin <lazcatluc@gmail.com>
2016-02-27 12:20:27 +0200

    add simple string behavior tests

--- src/test/java/ro/contezi/SimpleStringTest.java
+++ src/test/java/ro/contezi/SimpleStringTest.java
@@ -2,11 +2,34 @@ package ro.contezi;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 public class SimpleStringTest {
+	private static final String COMPLEX_STRING = "complex\\string";
+
 	@Test
 	public void convertsSimpleStringToItselfQuoted() throws Exception {
 		assertThat(new SimpleString().convert("some string")).isEqualTo("\\Qsome string\\E");
+	}
+
+	@Test
+	public void simpleStringRegExpMatchesSimpleString() throws Exception {
+		assertStringIsMatchedToItself("simple string");
+	}
+	
+	@Test
+	public void complexStringRegExpMatchesComplexString() throws Exception {
+		assertStringIsMatchedToItself(COMPLEX_STRING);
+	}
+	
+	@Test
+	public void complexStringIsNotMatchedByDefault() throws Exception {
+		assertThat(Pattern.compile(COMPLEX_STRING).matcher(COMPLEX_STRING).matches()).isFalse();
+	}
+	
+	private void assertStringIsMatchedToItself(String simpleString) {
+		assertThat(Pattern.compile(new SimpleString().convert(simpleString)).matcher(simpleString).matches()).isTrue();
 	}
 }
