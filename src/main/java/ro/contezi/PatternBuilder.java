ae52e5f2fd400872981d5c904f90b1e2aea30acf
catalin <lazcatluc@gmail.com>
2016-02-28 14:33:01 +0200

    make pattern builder immutable for reuse

--- src/main/java/ro/contezi/PatternBuilder.java
+++ src/main/java/ro/contezi/PatternBuilder.java
@@ -3,11 +3,11 @@ package ro.contezi;
 import java.util.regex.Pattern;
 
 public class PatternBuilder {
-	
-	private final StringBuilder pattern;
-	
+
+	private final String pattern;
+
 	private PatternBuilder(StringBuilder patternBuilder) {
-		this.pattern = patternBuilder;
+		this.pattern = patternBuilder.toString();
 	}
 
 	public static PatternBuilder anyCharacter() {
@@ -17,74 +17,62 @@ public class PatternBuilder {
 	public static PatternBuilder string(String string) {
 		return new PatternBuilder(new StringBuilder("\\Q").append(string).append("\\E"));
 	}
-	
+
 	public static PatternBuilder from(char start) {
 		return new PatternBuilder(new StringBuilder("[").append(start).append('-'));
 	}
-	
+
 	public static PatternBuilder maybe(String string) {
-		final PatternBuilder pattern = string(string);
-		pattern.paranthesize().append('?');
-		return pattern;
+		return new PatternBuilder(string(string).paranthesize().append('?'));
 	}
 
 	public PatternBuilder anyNumberOfTimes() {
-		paranthesize().append('*');
-		return this;
+		return new PatternBuilder(paranthesize().append('*'));
 	}
-	
+
 	public PatternBuilder atLeastOnce() {
-		paranthesize().append('+');
-		return this;
+		return new PatternBuilder(paranthesize().append('+'));
 	}
-	
+
 	public PatternBuilder times(int times) {
-		paranthesize().append('{').append(times).append('}');
-		return this;
+		return new PatternBuilder(paranthesize().append('{').append(times).append('}'));
 	}
-	
+
 	public PatternBuilder atLeastTimes(int times) {
-		paranthesize().append('{').append(times).append(',').append('}');
-		return this;
+		return new PatternBuilder(paranthesize().append('{').append(times).append(',').append('}'));
 	}
-	
+
 	public PatternBuilder betweenTimes(int atLeast, int atMost) {
-		paranthesize().append('{').append(atLeast).append(',').append(atMost).append('}');
-		return this;
+		return new PatternBuilder(
+				paranthesize().append('{').append(atLeast).append(',').append(atMost).append('}'));
 	}
-	
+
 	public PatternBuilder then(PatternBuilder anotherPattern) {
-		pattern.append(anotherPattern.pattern);
-		return this;
+		return new PatternBuilder(new StringBuilder(pattern).append(anotherPattern.pattern));
 	}
-	
+
 	public PatternBuilder then(PatternGroup helloGroup) {
-		pattern.append("\\k<").append(helloGroup.getName()).append('>');		
-		return this;
+		return new PatternBuilder(new StringBuilder(pattern).append("\\k<").append(helloGroup.getName()).append('>'));
 	}
-	
+
 	public Pattern build() {
 		return Pattern.compile(pattern.toString());
 	}
 
 	private StringBuilder paranthesize() {
-		return pattern.insert(0, "(?:").append(')');		
+		return new StringBuilder("(?:").append(pattern).append(')');
 	}
 
 	public PatternBuilder or(PatternBuilder anotherPattern) {
-		paranthesize().append('|').append(anotherPattern.paranthesize());
-		return this;
+		return new PatternBuilder(paranthesize().append('|').append(anotherPattern.paranthesize()));
 	}
 
 	public PatternBuilder to(char c) {
-		pattern.append(c).append(']');
-		return this;
+		return new PatternBuilder(new StringBuilder(pattern).append(c).append(']'));
 	}
 
-	public PatternGroup group() {
-		PatternGroup group = new PatternGroup();
-		pattern.insert(0, "(?<"+group.getName()+'>').append(')');	
-		return group;
+	public PatternBuilder group(PatternGroup group) {
+		return new PatternBuilder(new StringBuilder("(?<").append(group.getName()).append('>').append(pattern).append(')'));
 	}
 
 }
--- src/test/java/ro/contezi/GroupTest.java
+++ src/test/java/ro/contezi/GroupTest.java
@@ -7,31 +7,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GroupTest {
-	
+
 	private PatternBuilder pattern;
 	private PatternGroup helloGroup;
-	
+
 	@Before
 	public void setUp() {
-		pattern = string("he").then(string("l").atLeastOnce()).then(string("o"));
-		helloGroup = pattern.group();
-		pattern.then(string("world")).then(helloGroup);
+		helloGroup = new PatternGroup();
+		pattern = string("he").then(string("l").atLeastOnce()).then(string("o")).group(helloGroup).then(string("world"))
+				.then(helloGroup);
 	}
-	
+
 	@Test
 	public void matchesGroupedSearch() throws Exception {
 		assertThat(pattern.build().matcher("helloworldhello").matches()).isTrue();
 	}
-	
+
 	@Test
 	public void matchesGroupedSearchAfter() throws Exception {
-		PatternBuilder foo = string("f").then(string("o").atLeastOnce());
-		PatternGroup fooGroup = foo.group();
-		PatternBuilder initial = foo.then(string("bar")).then(fooGroup);
-
-		assertThat(initial.then(pattern).build().matcher("foobarfoohelloworldhello").matches()).isTrue();
+		PatternGroup fooGroup = new PatternGroup();
+		assertThat(string("f").then(string("o").atLeastOnce()).group(fooGroup).then(string("bar"))
+				.then(fooGroup).then(pattern).build().matcher("foobarfoohelloworldhello").matches()).isTrue();
 	}
-	
+
 	@Test
 	public void doesntMatchDifferentNumberOfLinHello() throws Exception {
 		assertThat(pattern.build().matcher("helloworldhellllo").matches()).isFalse();
